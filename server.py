c4c752832a4ffbb2176515ae214d8d02436acfa8
Charlie Conneely <charlieconneely.cc@gmail.com>
2020-12-30 14:16:01 +0000

    Imported flask.jsonify to return data from server.py

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
-# flask for web app.
-import flask as fl
-# numpy for numerical work.
 import numpy as np
+import flask as fl
+from flask import jsonify
 
-# Create a new web app.
 app = fl.Flask(__name__)
 
-# Add root route.
+# Set default route
 @app.route("/")
 def home():
     return app.send_static_file('index.html')
+
+@app.route('/api/power')
+def power():
+    data = {"value":np.random.uniform()}
+    return jsonify(data)
