0debe4397f560bbc84fcc8fa0becabff2c401005
Charlie Conneely <charlieconneely.cc@gmail.com>
2020-12-30 20:18:44 +0000

    loading model and returning hard-coded prediction

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import numpy as np
 import flask as fl
+import tensorflow.keras as kr
 from flask import jsonify
 
 app = fl.Flask(__name__)
@@ -9,7 +10,10 @@ app = fl.Flask(__name__)
 def home():
     return app.send_static_file('index.html')
 
-@app.route('/api/power')
+@app.route("/api/power")
 def power():
-    data = {"value":np.random.uniform()}
-    return jsonify(data)
+    loaded_model = kr.models.load_model('model.h5')
+    result = loaded_model.predict([10.0])
+    resultJSON = jsonify({"value":result.item(0)})
+    #data = {"value":np.random.uniform()}
+    return resultJSON
